cfb87789907f3d3052e670978ae45422d679c92d
Jhohan Silva <jhohanf.silva@gmail.com>
2019-12-26 09:44:01 -0500

    Revert "2019/12/26"
    This reverts commit b6df0eeff49f100a9c31cb727e7d0fff93212026.

--- TAX/scripts/tax015.js
+++ TAX/scripts/tax015.js
@@ -25,24 +25,13 @@ var tarjOperacoion_tax015 = IMask(
     document.getElementById('tarjOperacoion_tax015'),
     {
         mask: Date,
-        lazy: true,
-        pattern: 'Y/m/d',
+        lazy: false,
+        overwrite: false,
+        autofix: true,
         blocks: {
-            y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 },
-            m: { mask: IMask.MaskedRange, placeholderChar: 'm', from: 01, to: 12, maxLength: 2 },
-            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 01, to: 31, maxLength: 2 }
-        },
-        format: function (date) {
-            return moment(date).format("YYYY/MM/DD");
-        },
-        parse: function (str) {
-            var fecha = moment(str).format("YYYY/MM/DD");
-            if (fecha == "Invalid date") {
-                CON851('01', '01', null, 'error', 'error');
-            }
-            else {
-                return str;
-            }
+            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 1, to: 31, maxLength: 2 },
+            m: { mask: IMask.MaskedRange, placeholderChar: 'm', from: 1, to: 12, maxLength: 2 },
+            Y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 }
         }
     }
 );
@@ -51,26 +40,13 @@ var fechaAfil_tax015 = IMask(
     document.getElementById('fechaAfil_tax015'),
     {
         mask: Date,
-        lazy: true,
-        pattern: 'y/m/d',
-        overwrite: false,
-        autofix: false,
+        lazy: false,
+        overwrite: true,
+        autofix: true,
         blocks: {
-            y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 },
+            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 1, to: 31, maxLength: 2 },
             m: { mask: IMask.MaskedRange, placeholderChar: 'm', from: 1, to: 12, maxLength: 2 },
-            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 1, to: 31, maxLength: 2 }
-        },
-        format: function (date) {
-            return moment(date).format("YYYY/MM/DD");
-        },
-        parse: function (str) {
-            var fecha = moment(str).format("YYYY/MM/DD");
-            if (fecha == "Invalid date") {
-                CON851('01', '01', null, 'error', 'error');
-            }
-            else {
-                return str;
-            }
+            Y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 }
         }
     }
 );
@@ -79,26 +55,13 @@ var fechaRetiro_tax015 = IMask(
     document.getElementById('fechaRetiro_tax015'),
     {
         mask: Date,
-        lazy: true,
-        pattern: 'Y-m-d',
-        overwrite: false,
-        autofix: false,
+        lazy: false,
+        overwrite: true,
+        autofix: true,
         blocks: {
-            y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 },
+            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 1, to: 31, maxLength: 2 },
             m: { mask: IMask.MaskedRange, placeholderChar: 'm', from: 1, to: 12, maxLength: 2 },
-            d: { mask: IMask.MaskedRange, placeholderChar: 'd', from: 1, to: 31, maxLength: 2 }
-        },
-        format: function (date) {
-            return moment(date).format("YYYY/MM/DD");
-        },
-        parse: function (str) {
-            var fecha = moment(str).format("YYYY/MM/DD");
-            if (fecha == "Invalid date") {
-                CON851('01', '01', null, 'error', 'error');
-            }
-            else {
-                return str;
-            }
+            Y: { mask: IMask.MaskedRange, placeholderChar: 'y', from: 1900, to: 2999, maxLength: 4 }
         }
     }
 );
@@ -130,7 +93,6 @@ function _ventanaVehiculos_tax015(e) {
             },
             callback: function (data) {
                 document.getElementById('placa_tax015').value = data.PLACA.trim()
-                document.getElementById('propie_tax015').value = data['']
                 _enterInput('#placa_tax015');
             }
         });
@@ -218,6 +180,7 @@ function modalidades_tax015() {
 function costos_tax015() {
     postData({ datosh: datosEnvio() }, get_url("app/TAX/CON803.DLL"))
         .then(data => {
+            console.log(data)
             $_TAX015.COSTOS = data.COSTO;
             $_TAX015.COSTOS.pop();
             CON850(_evaluarNovedad_tax015);
@@ -255,75 +218,39 @@ function _validarPlaca_tax015() {
         })
 
     if (busqueda && $_TAX015.NOVEDAD != '7') {
-        var datos_envio = datosEnvio() + idPlaca.toUpperCase() + "|";
-        postData({ datosh: datos_envio }, get_url("app/TAX/TAX015-1.DLL"))
-            .then(data => {
-                cargarDatosTax015(data.split("|"));
-                console.log(data)
-            }).catch(err => {
-                console.log(err);
-                evaluarPlaca_tax015();
+        //  cargar datos vehiculo postData --> tax015-1.dll
+        if ($_TAX015.NOVEDAD == '8') {
+            evaluarPropietario_tax015();
+        } else {
+            CON850_P(function (e) {
+                if (e.id == 'S') {
+                    var datos_envio = datosEnvio();
+                    datos_envio += $_TAX015.NOVEDAD;
+                    datos_envio += "|";
+                    datos_envio += idPlaca + "|";
+                    postData({ datosh: datos_envio }, get_url("app/TAX/TAX015.DLL"))
+                        .then(data => {
+                            console.log(data)
+                            jAlert({ titulo: 'Notificacion', mensaje: "Eliminado correctamente" }, finProceso_tax015);
+                        })
+                } else {
+                    evaluarPlaca_tax015();
+                }
+            }, {
+                msj: '02'
             })
+        }
     } else {
         if ($_TAX015.NOVEDAD == '7' && !busqueda) {
-            producidoEmpresa_tax015.unmaskedValue = '0';
+            fondoReposicion_tax015.unmaskedValue = '0';
             fondoMantenim_tax015.unmaskedValue = '0';
             fondoReposicion_tax015.unmaskedValue = '0';
-            $("#combu_tax015").val('0').trigger('change');
-            $("#Naturaleza_tax015").val('0').trigger('change');
             evaluarPropietario_tax015();
         } else {
             plantillaToast('99', '01', null, 'warning');
             evaluarPlaca_tax015();
         }
     }
-}
-
-function cargarDatosTax015(data) {
-    document.getElementById("propie_tax015").value = data[1];
-    document.getElementById("marca_tax015").value = data[2];
-    document.getElementById("motor_tax015").value = data[3];
-    document.getElementById("chasis_tax015").value = data[4];
-    document.getElementById("modalidad_tax015").value = data[5];
-    document.getElementById("modelo_tax015").value = data[6];
-    document.getElementById("pasajeros_tax015").value = data[7];
-    producidoEmpresa_tax015.unmaskedValue = data[8];
-    fondoMantenim_tax015.unmaskedValue = data[9];
-    fondoReposicion_tax015.unmaskedValue = data[10];
-    $("#combu_tax015").val(data[11]).trigger('change');
-    $("#Naturaleza_tax015").val(data[12]).trigger('change');
-    // document.getElementById("combu_tax015").value = data[11];
-    // document.getElementById("Naturaleza_tax015").value = data[12];
-    document.getElementById("nroInterno_tax015").value = data[13];
-    tarjOperacoion_tax015.unmaskedValue = data[14];
-    fechaAfil_tax015.unmaskedValue = data[15];
-    fechaRetiro_tax015.unmaskedValue = data[16];
-    document.getElementById("costo_tax015").value = data[17];
-
-
-    //  cargar datos vehiculo postData --> tax015-1.dll
-    if ($_TAX015.NOVEDAD == '8') {
-        evaluarPropietario_tax015();
-    } else {
-        CON850_P(function (e) {
-            if (e.id == 'S') {
-                var datos_envio = datosEnvio();
-                datos_envio += $_TAX015.NOVEDAD;
-                datos_envio += "|";
-                datos_envio += document.getElementById('placa_tax015').value.toString().toUpperCase() + "|";
-                postData({ datosh: datos_envio }, get_url("app/TAX/TAX015.DLL"))
-                    .then(data => {
-                        console.log(data)
-                        jAlert({ titulo: 'Notificacion', mensaje: "Eliminado correctamente" }, finProceso_tax015);
-                    })
-            } else {
-                evaluarPlaca_tax015();
-            }
-        }, {
-            msj: '02'
-        })
-    }
-
 }
 
 function evaluarPropietario_tax015() {
@@ -340,7 +267,7 @@ function evaluarPropietario_tax015() {
 function _validarPropietario_tax015() {
     var idTercero = document.getElementById('propie_tax015').value,
         busqueda = $_TAX015.TERCEROS.find(e => {
-            return e.COD == idTercero.padStart(10, '0').toUpperCase();
+            return e.COD == idTercero.toUpperCase();
         })
     if (busqueda) {
         document.getElementById("propieDescrip_tax015").value = busqueda.NOMBRE
@@ -428,6 +355,7 @@ function evaluarFondoMantenimiento_tax015() {
 }
 
 function evaluarFondoRepos_tax015() {
+    console.log('repos')
     validarInputs(
         {
             form: "#faseFondoRepos",
@@ -474,6 +402,7 @@ function _validarNaturale_tax015(e) {
 }
 
 function evaluarNroInterno_tax015() {
+    console.log("nro interno")
     validarInputs(
         {
             form: "#faseNroInterno",
@@ -483,72 +412,19 @@ function evaluarNroInterno_tax015() {
             $('#Naturaleza_tax015').val("0")
             evaluarNaturaleza_tax015()
         },
-        evaluarTarjOperacion
+        evaluarFechas_tax015
     )
 }
 
-
-
-function evaluarTarjOperacion() {
+function evaluarFechas_tax015() {
+    console.log("fechas")
     validarInputs(
         {
-            form: "#faseTarjOperacion",
+            form: "#faseFechas",
             orden: "1"
         },
         evaluarNroInterno_tax015,
-        () => {
-            if (tarjOperacoion_tax015.unmaskedValue) {
-                if (!tarjOperacoion_tax015.value.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/)) {
-                    jAlert({ titulo: 'Error', mensaje: "Fecha incompleta" }, evaluarTarjOperacion);
-                } else {
-                    evalurarFechaAfil();
-                }
-            } else {
-                evalurarFechaAfil();
-            }
-        }
-    )
-}
-
-function evalurarFechaAfil() {
-    validarInputs(
-        {
-            form: "#faseFechaAfil",
-            orden: "1"
-        },
-        evaluarTarjOperacion,
-        () => {
-            if (fechaAfil_tax015.unmaskedValue) {
-                if (!fechaAfil_tax015.value.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/)) {
-                    jAlert({ titulo: 'Error', mensaje: "Fecha incompleta" }, evalurarFechaAfil);
-                } else {
-                    evaluarFechaRetiro();
-                }
-            } else {
-                evaluarFechaRetiro();
-            }
-        }
-    )
-}
-
-function evaluarFechaRetiro() {
-    validarInputs(
-        {
-            form: "#faseFechaRetiro",
-            orden: "1"
-        },
-        evalurarFechaAfil,
-        () => {
-            if (fechaRetiro_tax015.unmaskedValue) {
-                if (!fechaRetiro_tax015.value.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/)) {
-                    jAlert({ titulo: 'Error', mensaje: "Fecha incompleta" }, evaluarFechaRetiro);
-                } else {
-                    evaluarCosto_tax015();
-                }
-            } else {
-                evaluarCosto_tax015();
-            }
-        }
+        evaluarCosto_tax015
     )
 }
 
@@ -566,7 +442,8 @@ function evaluarCosto_tax015() {
 function _validarCosto_ta015() {
     var idCosto = document.getElementById('costo_tax015').value,
         busqueda = $_TAX015.COSTOS.find(e => {
-            return e.COD.trim() == idCosto.padStart(4, '0').toUpperCase();
+            console.log(e.COD)
+            return e.COD.trim() == idCosto.toUpperCase();
         })
 
     if (busqueda) {
@@ -578,13 +455,11 @@ function _validarCosto_ta015() {
                 datos_envio += "|";
                 datos_envio += bajarDatos_tax015();
                 console.log(datos_envio);
-                postData({ datosh: datos_envio }, get_url("app/TAX/TAX015.DLL"))
-                    .then(data => {
-                        console.log(data)
-                        jAlert({ titulo: 'Notificacion', mensaje: "Modificado correctamente" }, finProceso_tax015);
-                    }).catch(err => {
-                        evaluarCosto_tax015()
-                    })
+                // postData({ datosh: datos_envio }, get_url("app/TAX/TAX015.DLL"))
+                //     .then(data => {
+                //         console.log(data)
+                //         jAlert({ titulo: 'Notificacion', mensaje: "Modificado correctamente" }, finProceso_tax015);
+                //     })
             } else {
                 evaluarCosto_tax015();
             }
@@ -596,24 +471,24 @@ function _validarCosto_ta015() {
 }
 
 function bajarDatos_tax015() {
-    var fecha_tOperac = tarjOperacoion_tax015.value ? tarjOperacoion_tax015.value.split("/") : "00/00/00".split("/");
-    var fecha_afil = fechaAfil_tax015.value ? fechaAfil_tax015.value.split("/") : "00/00/00".split("/");
-    var fecha_retiro = fechaRetiro_tax015.value ? fechaRetiro_tax015.value.split("/") : "00/00/00".split("/");
+    var fecha_tOperac = tarjOperacoion_tax015._unmaskedValue.split(".")
+    var fecha_afil = fechaAfil_tax015._unmaskedValue.split(".");
+    var fecha_retiro = fechaRetiro_tax015._unmaskedValue.split(".");
 
-    let fondo_car = fondoMantenim_tax015.unmaskedValue || 0;
-    fondo_car = parseFloat(fondo_car).toFixed(1).replace(/\./g, '');
-    fondo_car = fondo_car.padStart(2, "0")
+    let fondoCar = producidoEmpresa_tax015.unmaskedValue || 0;
+    fondoCar = parseFloat(fondoCar).toFixed(2).replace(/\./g, '');
+    fondoCar = fondoCar.padStart(4, "0");
 
-    let fondo_r = fondoReposicion_tax015.unmaskedValue || 0;
+    let fondo_r = fondoMantenim_tax015.unmaskedValue || 0;
     fondo_r = parseFloat(fondo_r).toFixed(1).replace(/\./g, '');
-    fondo_r = fondo_r.padStart(2, "0");
+    fondo_r = fondo_r.padStart(2, "0")
 
-    let producido_empresa = producidoEmpresa_tax015.unmaskedValue || 0;
-    producido_empresa = parseFloat(producido_empresa).toFixed(2).replace(/\./g, '');
-    producido_empresa = producido_empresa.padStart(4, "0");
+    let fondo_reposicion = fondoReposicion_tax015.unmaskedValue || 0;
+    fondo_reposicion = parseFloat(fondo_reposicion).toFixed(1).replace(/\./g,'');
+    fondo_reposicion = fondo_reposicion.padStart(2,"0");
 
 
-    return document.getElementById('placa_tax015').value.toString().toUpperCase()
+    return document.getElementById('placa_tax015').value
         + "|"
         + document.getElementById('propie_tax015').value
         + "|"
@@ -631,35 +506,28 @@ function bajarDatos_tax015() {
         + "|"
         + document.getElementById('chasis_tax015').value
         + "|"
-        + fecha_tOperac[0].padStart(4, "0") + fecha_tOperac[1].padStart(2, "0") + fecha_tOperac[2].padStart(2, "0")
+        + fecha_tOperac[2].padStart(4, "0") + fecha_tOperac[1].padStart(2, "0") + fecha_tOperac[0].padStart(2, "0")
         + "|"
         + document.getElementById('Naturaleza_tax015').value
         + "|"
-        + fondo_car
+        + fondoCar
         + "|"
         + fondo_r
         + "|"
-        + document.getElementById('modalidad_tax015').value.toString().toUpperCase()
+        + document.getElementById('modalidad_tax015').value
         + "|"
-        + document.getElementById('costo_tax015').value.padStart(4, "0")
+        + document.getElementById('costo_tax015').value
         + "|"
-        + fecha_afil[0].padStart(4, "0") + fecha_afil[1].padStart(2, "0") + fecha_afil[2].padStart(2, "0")
+        + fecha_afil[2].padStart(4, "0") + fecha_afil[1].padStart(2, "0") + fecha_afil[0].padStart(2, "0")
         + "|"
-        + fecha_retiro[0].padStart(4, "0") + fecha_retiro[1].padStart(2, "0") + fecha_retiro[2].padStart(2, "0")
+        + fecha_retiro[2].padStart(4, "0") + fecha_retiro[1].padStart(2, "0") + fecha_retiro[0].padStart(2, "0")
         + "|"
-        + producido_empresa
+        + fondo_reposicion
         + "|";
 }
 
 function finProceso_tax015() {
     _inputControl('reset');
     _inputControl('disabled');
-    $_TAX015 = {
-        NOVEDAD: false,
-        PLACAS: [],
-        TERCEROS: [],
-        MODALIDADES: [],
-        COSTOS: []
-    }
     placas_tax015();
 }
