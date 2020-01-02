5bdb5e2ba963dc777d7f6274fb33d5f17060e28e
gabrielbohorquez <gabrielbohorquezr@hotmail.com>
2020-01-02 10:33:29 -0500

    avance

--- SALUD/scripts/SAL491.js
+++ SALUD/scripts/SAL491.js
@@ -1,17 +1,52 @@
-var arrayCompleto_sal491 = []
+var f8comprob_sal491 = []
 var global_SAL491 = []
+var claseServicio_491 = [
+    {"codigo": "0", "descripcion": "DROGUERIA"},
+    {"codigo": "1", "descripcion": "CIRUGIAS"},
+    {"codigo": "2", "descripcion": "LAB. Y OTROS DIAG."},
+    {"codigo": "3", "descripcion": "RX - IMAGENOLOGIA"},
+    {"codigo": "4", "descripcion": "OTROS SERVICIOS"},
+    {"codigo": "5", "descripcion": "CONSULTAS Y TERAPIAS"},
+    {"codigo": "6", "descripcion": "PATOLOGIA"},
+    {"codigo": "7", "descripcion": "PROMOCION Y PREVENCION"}]
 
 $(document).ready(function () {
     _inputControl("reset");
     _inputControl("disabled");
     _toggleF8([
-        { input: 'claseservicio', app: 'SAL41', funct: _ventanaClases_41 },
+        { input: 'claseservicio', app: 'SAL491', funct: _ventanaClases_491 },
     ]);
     global_SAL491['PREFIJOUSU'] = $_USUA_GLOBAL[0].PREFIJ;
     global_SAL491['ADMIN_W'] = localStorage.getItem('Usuario').trim() ? localStorage.getItem('Usuario').trim() : false;
     global_SAL491['NITUSU'] = $_USUA_GLOBAL[0].NIT.toString().padStart(10, '0');
     inicio_sal491()
 })
+
+function _ventanaClases_491(e){
+    if (e.type == "keydown" && e.which == 119 || e.type == 'click') {
+        _ventanaDatos({
+            titulo: 'TIPO DE SERVICIO',
+            columnas: ["codigo", "descripcion"],
+            data: claseServicio_491,
+            callback_esc: function () {
+                $('#claseservicio_SAL491').focus()
+            },
+            callback: function (data) {
+                $('#claseservicio_SAL491').val(data.codigo)
+                _enterInput('#claseservicio_SAL491');
+            }
+        });
+    }
+}
+
+function cerrarArchivos_491(){
+    _inputControl("reset");
+    _inputControl("disabled");
+    claseServicio_491 = []
+    global_SAL491 = []
+    f8comprob_sal491 = []
+    _toggleNav()
+}
 
 function inicio_sal491() {
     var datos_envio_491 = datosEnvio()
@@ -26,9 +61,9 @@ function inicio_sal491() {
         .then((data) => {
             loader("hide")
             var date = data.split('|');
-            global_SAL491['NOMBREOPERW'] = date[0].trim();
-            global_SAL491['IDENTOPERW'] = date[1].trim();
-            global_SAL491['SUCOPERW'] = date[2].trim();
+            global_SAL491['NOMBRE_OPER'] = date[0].trim();
+            global_SAL491['IDENT_OPER'] = date[1].trim();
+            global_SAL491['SUC_OPER'] = date[2].trim();
             datoSucursal_491()
         })
         .catch(error => {
@@ -40,24 +75,110 @@ function inicio_sal491() {
 function datoSucursal_491() {
     if (global_SAL491.PREFIJOUSU == "  ") {
         global_SAL491.PREFIJOUSU = "00";
-        _Datosucursal2_41();
+        claseServicio_SAL491();
     }
-    else if ((global_SAL491.ADMIN_W == "GEBC") || (global_SAL491.ADMIN_W == "ADMI")) {
-        _Datosucursal2_41();
+
+    if ((global_SAL491.ADMIN_W == "GEBC") || (global_SAL491.ADMIN_W == "ADMI")) {
+        $('#unidades_SAL491').val(global_SAL491.PREFIJOUSU)
+        claseServicio_SAL491();
     }
     else {
         switch (global_SAL491.NITUSU) {
-           // ESE SALUD YOPAL
-           case "0844003225":
-            if ((global_SAL491['SUCOPERW'] == "JL") || (global_SAL491['SUCOPERW'] == "CA") || (global_SAL491['SUCOPERW'] == "CS") || (global_SAL491['SUCOPERW'] == "PV") || (global_SAL491['SUCOPERW'] == "BC") || (global_SAL491['SUCOPERW'] == "LC") || (global_SAL491['SUCOPERW'] == "CV") || (global_SAL491['SUCOPERW'] == "HT") || (global_SAL491['SUCOPERW'] == "EM") || (global_SAL491['SUCOPERW'] == "HY") || (global_SAL491['SUCOPERW'] == "TL") || (global_SAL491['SUCOPERW'] == "MR")) {
-                global_SAL491.PREFIJOUSU = $_PREFIJOUSU;
-                _Datosucursal2_41();
-            }
-            else {
-                _inputControl("disabled");
-                CON852("48", "Sucursal no permitida", "_infodatosucursal", _toggleNav);
-            }
-            break;
+            // ESE SALUD YOPAL
+            case "0844003225":
+                if ((global_SAL491.SUC_OPER == "JL") || (global_SAL491.SUC_OPER == "CA") || (global_SAL491.SUC_OPER == "CS") || (global_SAL491.SUC_OPER == "PV") || (global_SAL491.SUC_OPER == "BC") || (global_SAL491.SUC_OPER == "LC") || (global_SAL491.SUC_OPER == "CV") || (global_SAL491.SUC_OPER == "HT") || (global_SAL491.SUC_OPER == "EM") || (global_SAL491.SUC_OPER == "HY") || (global_SAL491.SUC_OPER == "TL") || (global_SAL491.SUC_OPER == "MR")) {
+                    global_SAL491.PREFIJOUSU = global_SAL491.SUC_OPER
+                }
+                else {
+                    CON851('48', '48', null, 'error', 'error');
+                    cerrarArchivos_491()
+                }
+                break;
+            // SERVIMEDICOS
+            case "0800162035":
+                if ((global_SAL491.SUC_OPER == "01") || (global_SAL491.SUC_OPER == "03") || (global_SAL491.SUC_OPER == "05") || (global_SAL491.SUC_OPER == "06") || (global_SAL491.SUC_OPER == "07") || (global_SAL491.SUC_OPER == "08") || (global_SAL491.SUC_OPER == "10") || (global_SAL491.SUC_OPER == "11") || (global_SAL491.SUC_OPER == "12") || (global_SAL491.SUC_OPER == "14") || (global_SAL491.SUC_OPER == "15")) {
+                    global_SAL491.PREFIJOUSU = global_SAL491.SUC_OPER
+                }
+                else {
+                    CON851('48', '48', null, 'error', 'error');
+                    cerrarArchivos_491()
+                }
+                break;
+            // FAMEDIC
+            case "0900405505":
+                if ((global_SAL491.SUC_OPER == "01") || (global_SAL491.SUC_OPER == "02") || (global_SAL491.SUC_OPER == "03") || (global_SAL491.SUC_OPER == "04") || (global_SAL491.SUC_OPER == "05") || (global_SAL491.SUC_OPER == "06")) {
+                    global_SAL491.PREFIJOUSU = global_SAL491.SUC_OPER
+                }
+                else {
+                    CON851('48', '48', null, 'error', 'error');
+                    cerrarArchivos_491()
+                }
+                break;
         }
+
     }
+    $('#unidades_SAL491').val(global_SAL491.PREFIJOUSU)
+    global_SAL491['SUC_W'] = global_SAL491.PREFIJOUSU
+    claseServicio_SAL491();
+}
+
+function sucursal_SAL491() {
+    validarInputs(
+        {
+            form: '#SU_491',
+            orden: '1'
+        },
+        function () {
+            cerrarArchivos_491();
+        },
+        function () {
+            global_SAL491.SUC_W = $('#unidades_SAL491').val()
+            claseServicio_SAL491()
+        }
+    )
+}
+
+function claseServicio_SAL491() {
+    validarInputs(
+        {
+            form: '#SERVICE_491',
+            orden: '1'
+        },
+        function () {
+            cerrarArchivos_491();
+        },
+        function () {
+            global_SAL491['CL_W'] = $('#claseservicio_SAL491').val()
+
+            if ((global_SAL491['CL_W'] == 0) && ($_USUA_GLOBAL[0]['SEG-MOV'] == "3")) {
+                CON851B($_USUA_GLOBAL[0]['SEG-MOV']);
+                sucursal_SAL491()
+            } else {
+                var Servicio = claseServicio_491.find(servicio => servicio.codigo == global_SAL491.CL_W)
+
+                if (Servicio){
+                    $('#claseservicio_SAL491').val(Servicio.codigo + ' - ' + Servicio.descripcion)
+                    nroComprob_491()
+                } else {
+                    CON851('01', '01', null, 'error', 'error');
+                    claseServicio_SAL491()
+                }
+            }
+        }
+    )
+}
+
+function nroComprob_491(){
+    validarInputs(
+        {
+            form: '#SERVICE_491',
+            orden: '1'
+        },
+        function () {
+            cerrarArchivos_491();
+        },
+        function () {
+            
+        }
+    )
 }
