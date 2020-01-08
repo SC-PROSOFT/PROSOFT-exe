c6333e04e0845c09b61c467e27f3b525017d9cba
Camilo Franco <camiloes93@hotmail.com>
2020-01-08 17:39:09 -0500

    Nuevo loader para tipo de empresa H --add

--- frameworks/scripts/datosCompletos.js
+++ frameworks/scripts/datosCompletos.js
@@ -1,268 +1,132 @@
-function obtenerDatosCompletos(data, callbackSucces, callbackError) {
-    var nombreFD = data.nombreFd.toUpperCase()
-    var programa
-    var objLocal = ''
+(function ($) {
+    $.datoscompletos = {
+        dll: [],
+        callback: null,
+        callbackerror: null,
+        objLocal: '',
+        estado: null,
 
-    // var ventanaimpresion = bootbox.dialog({
-    //     message: '<div class="text-center"><div><i class="fa fa-spin fa-spinner"></i> Cargando ' + nombreFD + '...</div></div>',
-    //     closeButton: false,
-    //     buttons: {
-    //         confirm: {
-    //             label: 'Aceptar',
-    //             className: 'btn-success',
-    //             callback: function () {
+        _init: (dll, callbackSucces, estado, callbackError) => {
+            dll == undefined ? $.datoscompletos.dll = console.error('No tiene ningún DLL') : $.datoscompletos.dll = dll;
+            estado == undefined ? $.datoscompletos.estado = '' : $.datoscompletos.estado = estado.toUpperCase();
+            $.datoscompletos.callback = callbackSucces;
+            callbackError == undefined ? $.datoscompletos.callbackerror = _toggleNav : $.datoscompletos.callbackerror = callbackError;
 
-    //             }
-    //         }
-    //     }
-    // })
-    // ventanaimpresion.init($('.modal-footer').hide());
-    switch (nombreFD) {
-        case 'ENFERMEDADES':
-            programa = "SALUD/SER851"
-            // no requiere filtro
-            break;
-        case 'PACIENTES':
-            programa = "SALUD/SER810"
-            break;
-        case 'ENTIDADES':
-            programa = "SALUD/SER853"
-            break;
-        case 'OCUPACIONES':
-            programa = "SALUD/SER854"
-            break;
-        case 'CIUDADES':
-            programa = "CONTAB/CON809"
-            break;
-        case 'PROFESIONALES':
-            programa = "SALUD/SER819"
-            break;
-        case 'TERCEROS':
-            programa = "CONTAB/CON802"
-            break;
-        case 'DIVISION':
-            programa = "INVENT/INV809-03"
-            break;
-        case 'IPS':
-            programa = "SALUD/SER813"
-            break;
-        case 'NUMERACION':
-            programa = "SALUD/SER808"
-            break;
-        case 'SERV_HOSP':
-            programa = "SALUD/SER812"
-            break;
-        case 'ZONAS_RUTAS':
-            programa = "CONTAB/CON810"
-            // SI REQUIERE FILTRO
-            break;
-        case 'PATOLOGIAS':
-            programa = "SALUD/SER858"
-            break;
-        case 'PAISES_RIPS':
-            programa = "SALUD/SER888"
-            break;
-        case 'UNSERV':
-            programa = "SALUD/SER873"
-            break;
-        case 'COSTOS':
-            programa = "CONTAB/CON803"
-            break;
-        case 'ARTICULOS':
-            programa = "INVENT/INV803"
-            break;
-        case 'TABLAS':
-            programa = "SALUD/SER802"
-            break;
-        case 'GRUPO-SER':
-            programa = "SALUD/SER801"
-            break;
-        case 'CTA-MAYOR':
-            programa = "CONTAB/CON801"
-            break;
-        // case 'NOM-TAR':
-        //     programa = "SALUD/SAL712-01"
-        //     break;     DLL ES IGUAL SER803  F8 NOMTAR 
-        case 'ETNIAS':
-            programa = "SALUD/SER867"
-            break;
-        // case 'PAIRIP':
-        //     programa = "SALUD/SER888"
-        //     break;   DLL REPETIDO
-        case 'COLEGIOS':
-            programa = "SALUD/SER902"
-            break;
-        case 'CLASIPACI':
-            programa = "SALUD/SER868"
-            break;
-        //REVISION DIANA            
-        case 'PREFIJOS':
-            programa = "INVENT/INV109"
-            // NO REQUIERE FILTRO
-            break;
-        case 'LOTES':
-            programa = "CONTAB/CON110I"
-            // NO REQUIERE FILTRO
-            break;
-        case 'GRUPOTAR':
-            programa = "SALUD/SER803"
-            // NO REQUIERE FILTRO
-            break;
-        case 'LOCALIZACION':
-            programa = "INVENT/INV801"
-            // NO REQUIERE FILTRO
-            break;
-        case 'GRNEGOCIO':
-            programa = "CONTAB/CON818"
-            // NO REQUIERE FILTRO
-            break;
-        case 'CLASC':
-            programa = "CONTAB/CON810S"
-            // NO REQUIERE FILTRO
-            break;
-        case 'VENDEDOR':
-            programa = "CONTAB/CON805"
-            // NO REQUIERE FILTRO
-            break;
-        case 'USO':
-            programa = "INVENT/INV806"
-            // SI REQUIERE FILTRO
-            break;
-        case 'POLIT':
-            programa = "INVENT/INV807"
-            // NO REQUIERE FILTRO
-            break;
-        case 'GRUPOS':
-            programa = "INVENT/INV804"
-            // SI REQUIERE FILTRO
-            break;
-        case 'COSTO':
-            programa = "CONTAB/CON803-01"
-            // NO REQUIERE FILTRO
-            break;
-        case 'ESPECIALIDAD':
-            programa = "SALUD/SER855"
-            // NO REQUIERE FILTRO
-            break;
-        case 'SUCURSALES':
-            programa = "CONTAB/CON823"
-            // NO REQUIERE FILTRO
-            break;
-        case 'OPERADOR':
-            programa = "CONTAB/CON982"
-            // NO REQUIERE FILTRO
-            break;
-        case 'PREMED':
-            programa = "SALUD/SER103"
-            // SI REQUIERE FILTRO
-            break;
-        case 'MEDATC':
-            programa = "SALUD/SER857"
-            // NO REQUIERE FILTRO
-            break;
-        case 'GRCAPITA':
-            programa = "SALUD/SER871"
-            // NO REQUIERE FILTRO
-            break;
-        case 'COMUNIDADES':
-            programa = "SALUD/SER116A"
-            // NO REQUIERE FILTRO
-            break;
-        case 'RESGUARDOS':
-            programa = "SALUD/SER117A"
-            // NO REQUIERE FILTRO
-            break;
-        case 'EQUIOPERADOR':
-            programa = "SALUD/SER11V"
-            // NO REQUIERE FILTRO
-            break;
-        case 'TARIFAS':
-            programa = "SALUD/SER804"
-            // NO REQUIERE FILTRO
-            break;
-        case 'DESPLAZADO':
-            programa = "SALUD/SER810D"
-            // NO REQUIERE FILTRO
-            break;
-        case 'PROFESION':
-            objLocal = {
-                'PROFESION': [
-                    { 'COD': '1', 'DESCRIP': 'MEDICO ESPECIALISTA' },
-                    { 'COD': '2', 'DESCRIP': 'MEDICO GENERAL' },
-                    { 'COD': '3', 'DESCRIP': 'ENFERMERO(A) JEFE' },
-                    { 'COD': '4', 'DESCRIP': 'AUXILIAR ENFERMERIA' },
-                    { 'COD': '5', 'DESCRIP': 'TERAPEUTAS Y OTROS' },
-                    { 'COD': '6', 'DESCRIP': 'ENFERMERA JEFE P Y P' },
-                    { 'COD': '7', 'DESCRIP': 'PSICOLOGIA' },
-                    { 'COD': '8', 'DESCRIP': 'NUTRICIONISTA' },
-                    { 'COD': '9', 'DESCRIP': 'NO APLICA' },
-                    { 'COD': 'A', 'DESCRIP': 'ODONTOLOGO' },
-                    { 'COD': 'B', 'DESCRIP': 'AUDITOR MEDICO' },
-                    { 'COD': 'H', 'DESCRIP': 'HIGIENE ORAL' },
-                    { 'COD': 'I', 'DESCRIP': 'INSTRUMENTADOR(A)' },
-                    { 'COD': 'O', 'DESCRIP': 'OPTOMETRA' },
-                    { 'COD': 'T', 'DESCRIP': 'TRABAJO SOCIAL' },
-                ]
+            $.datoscompletos._consulta(dll);
+            $.datoscompletos._cargando();
+        },
+
+        _consulta: (dll) => {
+            DLLS = {
+                'ENFERMEDADES':  'SALUD/SER851', 
+                'PACIENTES':     'SALUD/SER810',
+                'ENTIDADES':     'SALUD/SER853',
+                'OCUPACIONES':   'SALUD/SER854',
+                'CIUDADES':      'CONTAB/CON809',
+                'PROFESIONALES': 'SALUD/SER819',
+                'TERCEROS':      'CONTAB/CON802',
+                'DIVISION':      'INVENT/INV809-03',
+                'IPS':           'SALUD/SER813',
+                'NUMERACION':    'SALUD/SER808',
+                'SERV_HOSP':     'SALUD/SER812',
+                'ZONAS_RUTAS':   'CONTAB/CON810', // SI REQUIERE FILTRO
+                'PATOLOGIAS':    'SALUD/SER858',
+                'PAISES_RIPS':   'SALUD/SER888',
+                'UNSERV':        'SALUD/SER873',
+                'COSTOS':        'CONTAB/CON803',
+                'ARTICULOS':     'INVENT/INV803',
+                'TABLAS':        'SALUD/SER802',
+                'GRUPO-SER':     'SALUD/SER801',
+                'CTA-MAYOR':     'CONTAB/CON801',
+                'ETNIAS':        'SALUD/SER867',
+                'COLEGIOS':      'SALUD/SER902',
+                'CLASIPACI':     'SALUD/SER868',
+                'PREFIJOS':      'INVENT/INV109',
+                'LOTES':         'CONTAB/CON110I',
+                'GRUPOTAR':      'SALUD/SER803',
+                'LOCALIZACION':  'INVENT/INV801',
+                'GRNEGOCIO':     'CONTAB/CON818',
+                'CLASC':         'CONTAB/CON810S',
+                'VENDEDOR':      'CONTAB/CON805',
+                'USO':           'INVENT/INV806', // SI REQUIERE FILTRO
+                'POLIT':         'INVENT/INV807',
+                'GRUPOS':        'INVENT/INV804', // SI REQUIERE FILTRO
+                'COSTO':         'CONTAB/CON803-01',
+                'ESPECIALIDAD':  'SALUD/SER855',
+                'SUCURSALES':    'CONTAB/CON823',
+                'OPERADOR':      'CONTAB/CON982',
+                'PREMED':        'SALUD/SER103', // SI REQUIERE FILTRO
+                'MEDATC':        'SALUD/SER857',
+                'GRCAPITA':      'SALUD/SER871',
+                'COMUNIDADES':   'SALUD/SER116A',
+                'RESGUARDOS':    'SALUD/SER117A',
+                'EQUIOPERADOR':  'SALUD/SER11V',
+                'TARIFAS':       'SALUD/SER804',
+                'DESPLAZADO':    'SALUD/SER810D',
+                'ESPCUPS':       'SALUD/SAL71A-C', // se puede filtrar por cod-cups
+                'CUPS':          'SALUD/SER802C',
+                'PROFESION' : {
+                                 'PROFESION': [
+                                 { 'COD': '1', 'DESCRIP': 'MEDICO ESPECIALISTA' },
+                                 { 'COD': '2', 'DESCRIP': 'MEDICO GENERAL' },
+                                 { 'COD': '3', 'DESCRIP': 'ENFERMERO(A) JEFE' },
+                                 { 'COD': '4', 'DESCRIP': 'AUXILIAR ENFERMERIA' },
+                                 { 'COD': '5', 'DESCRIP': 'TERAPEUTAS Y OTROS' },
+                                 { 'COD': '6', 'DESCRIP': 'ENFERMERA JEFE P Y P' },
+                                 { 'COD': '7', 'DESCRIP': 'PSICOLOGIA' },
+                                 { 'COD': '8', 'DESCRIP': 'NUTRICIONISTA' },
+                                 { 'COD': '9', 'DESCRIP': 'NO APLICA' },
+                                 { 'COD': 'A', 'DESCRIP': 'ODONTOLOGO' },
+                                 { 'COD': 'B', 'DESCRIP': 'AUDITOR MEDICO' },
+                                 { 'COD': 'H', 'DESCRIP': 'HIGIENE ORAL' },
+                                 { 'COD': 'I', 'DESCRIP': 'INSTRUMENTADOR(A)' },
+                                 { 'COD': 'O', 'DESCRIP': 'OPTOMETRA' },
+                                 { 'COD': 'T', 'DESCRIP': 'TRABAJO SOCIAL' },
+                              ]},
             }
-            break;
-        case 'ESPCUPS':
-            programa = "SALUD/SAL71A-C"
-            // se puede filtrar por cod-cups
-            break;
-        case 'CUPS':
-            programa = "SALUD/SER802C"
-            break;
-        default:
-            $('.btn-success').click();
-            jAlert(
-                { titulo: 'Error', mensaje: 'Nombre de fd no válido' },
-                _toggleNav
-            );
-            break;
+            let ruta = DLLS[dll.nombreFd.toUpperCase()];
+            let datos_envio = datosEnvio();
+            if (dll.filtro) {
+                datos_envio += dll.filtro + '|';
+                if (dll.campo) datos_envio += dll.campo + '|';
+            }
+            if (typeof ruta === 'object') {
+                $.datoscompletos.callback(ruta);
+            } else {
+                let URL = get_url("APP/" + ruta + ".DLL");
+                postData({
+                    datosh: datos_envio
+                }, URL)
+                    .then(data => {
+                        console.log(data)
+                        $.datoscompletos.callback(data);
+                        if ($.datoscompletos.estado == 'ONLY') {
+                            loader('hide');
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error)
+                        $.datoscompletos.callbackerror();
+                    });
+            }
+        },
 
-    }
-
-    // var prueba = {
-    //     CUPS: 'SALUD/SER802C', // FDFDF
-    //     ESPCUPS: 'SALUD'
-    // }
-
-    // if(prueba[$variable] ){
-
-    // }else{
-
-    // }
-
-    if (objLocal == '') {
-        var datos_envio_DC = datosEnvio();
-        if (data.filtro) {
-            datos_envio_DC += data.filtro
-            datos_envio_DC += '|'
-            if (data.campo) {
-                datos_envio_DC += data.campo
-                datos_envio_DC += '|'
+        _cargando: () => {
+            switch ($.datoscompletos.estado) {
+                case 'ON':
+                    loader('show');
+                    break;
+                case 'OFF':
+                    loader('hide');
+                    break;
+                case 'ONLY':
+                    loader('show');
+                    break;
             }
         }
 
-        let URL = get_url("APP/" + programa + ".DLL");
-
-
-        postData({
-            datosh: datos_envio_DC
-        }, URL)
-            .then((data) => {
-                callbackSucces(data);
-                // $('.btn-success').click();
-            })
-            .catch(error => {
-                console.error(error)
-                callbackError(callbackError)
-                // $('.btn-success').click();
-                // _toggleNav()
-            });
-    } else {
-        // $('.btn-success').click();
-        callbackSucces(objLocal);
-        objLocal = ''
-    }
-}
+    },
+        obtenerDatosCompletos = function (dll, callbackSucces, estado, callbackError) {
+            $.datoscompletos._init(dll, callbackSucces, estado, callbackError);
+        }
+})(jQuery);
--- frameworks/scripts/menu_user.js
+++ frameworks/scripts/menu_user.js
@@ -74,6 +74,15 @@ function _onCargarUsuario(data) {
                 var arrayEliminar = [];
                 arrayEliminar.push(json);
                 _eliminarJson(arrayEliminar, _onEliminarJsonUSU);
+                if ($_USUA_GLOBAL[0].TIPO_EMPRE == 'H') {
+                    $('.loader').html(
+                        '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-466.4 259.6 280.2 47.3" enable-background="new -466.4 259.6 280.2 47.3" xml:space="preserve" style="width: 50%; display: block; margin: 0 auto;">' +
+                        '<polyline fill="none" stroke="#476fAD" class="ekg" stroke-width="1" stroke-linecap="square" stroke-miterlimit="10" points="-465.4,281 -436,281 -435.3,280.6 -431.5,275.2 -426.9,281 -418.9,281 -423.9,281 -363.2,281 -355.2,269 -345.2,303 -335.2,263 -325.2,291 -319.2,281 -187.2,281 "/>' +
+                        '</svg>'
+                    )
+                    $('.loader').css('align-items', 'center');
+                    $('.wrapper').css('display', 'contents', 'height', '');
+                }
             },
             url
         );
