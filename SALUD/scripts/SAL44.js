d5bd11bfc7c04eb0646c45924a2c627ca1c2f8f3
Diana E <dianaesgon27@gmail.com>
2020-01-20 09:26:07 -0500

    opcion de reconsolidacion comprobante por prefijo

--- SALUD/scripts/SAL44.js
+++ SALUD/scripts/SAL44.js
@@ -2,8 +2,6 @@
 
 var SAL44 = [];
 
-
-
 $(document).ready(function () {
     _inputControl('reset');
     _inputControl('disabled');
@@ -12,7 +10,7 @@ $(document).ready(function () {
     $_FECHA_LNK = $_USUA_GLOBAL[0].FECHALNK;
     $_MESLNK = $_FECHA_LNK.substring(2, 4);
     $_ANOLNK = $_FECHA_LNK.substring(0, 2);
-    $_ANOLNK = 20 + $_ANOLNK; 
+    $_ANOLNK = 20 + $_ANOLNK;
     $_NITUSU = $_USUA_GLOBAL[0].NIT;
     $_PREFIJOUSU = $_USUA_GLOBAL[0].PREFIJ;
     $_CONTADOUSU = $_USUA_GLOBAL[0].CONTADO;
@@ -29,13 +27,11 @@ $(document).ready(function () {
     $_CODCIUUSU = $_USUA_GLOBAL[0].CONTROL_USU
     $_DPTCIUUSU = $_CODCIUUSU.substring(0, 1);
     $_CIUCIUUSU = $_CODCIUUSU.substring(1, 5);
-
+    $_INVENTUSU = $_USUA_GLOBAL[0].INVENT;
 
     _toggleF8([
         { input: 'prefijo', app: '9441', funct: _ventanaprefijo_9441 },
     ]);
-
-
     _evaluarprefijofact_sal44();
 
 });
@@ -100,7 +96,7 @@ function _evaluarprefijofact_sal44() {
                 case "J":
                     $('#descrippref_9441').val('AMBULATORIO');
                 case "K":
-                    $('#descrippref_9441').val('AMBULATORIO'); 
+                    $('#descrippref_9441').val('AMBULATORIO');
                 case "L":
                     $('#descrippref_9441').val('AMBULATORIO');
                 case "M":
@@ -128,9 +124,9 @@ function _evaluarprefijofact_sal44() {
                 case "T":
                     $('#descrippref_9441').val('AMBULATORIO');
                     _evaluarfechaini_sal44();
-                    break; 
+                    break;
                 default:
-                    _evaluarprefijofact_sal44(); 
+                    _evaluarprefijofact_sal44();
                     break;
             }
 
@@ -140,63 +136,137 @@ function _evaluarprefijofact_sal44() {
 
 
 function _evaluarfechaini_sal44() {
-    momentMaskfechainicial.updateValue();
+    $('#anoini_9441').val($_ANOLNK);
+    $('#mesini_9441').val($_MESLNK);
     validarInputs(
         {
-            form: "#FECHAINIC_9441",
+            form: "#DIAINIC_9441",
             orden: '1'
         },
         () => { _evaluarprefijofact_sal44(); },
         () => {
-            SAL44.FECHAINICIAL = momentMaskfechainicial.unmaskedValue;
+            SAL44.DIAINI = $('#diaini_9441').val();
+            SAL44.DIAINI = SAL44.DIAINI.padStart(2, '0');
+            SAL44.FECHAINICIAL = $_ANOLNK + $_MESLNK + SAL44.DIAINI;
             console.log(SAL44.FECHAINICIAL, 'SAL44.FECHAINICIAL')
-            LLAMADO_DLL({
-                dato: [SAL44.FECHAINICIAL],
-                callback: _consultafechaini_sal44,
-                nombredll: 'SAL44-01',
-                carpeta: 'SALUD'
-            });
-
+            _evaluarfechasal_sal44();
         }
     )
 }
 
-function _consultafechaini_sal44(data){
-    console.log(data, 'fecha inicial result')
-    var date = data.split("|");
-    var swinvalid = date[0].trim();
-    if (swinvalid == "00") {
-        _evaluarfechasal_sal44(); 
-    } else if (swinvalid == "01") {
-        CON851('01', '01', null, 'error', 'error');
-        _evaluarfechaini_sal44();
-    }
-}
-
-function _evaluarfechasal_sal44(){
-    momentMaskfechafinal.updateValue();
+function _evaluarfechasal_sal44() {
+    $('#anofin_9441').val($_ANOLNK);
+    $('#mesfin_9441').val($_MESLNK);
     validarInputs(
         {
-            form: "#FECHAFINAL_9441",
+            form: "#DIAFINAL_9441",
             orden: '1'
         },
         () => { _evaluarfechaini_sal44(); },
         () => {
+            SAL44.DIAFIN = $('#diafin_9441').val();
+            SAL44.FECHAFINAL = $_ANOLNK + $_MESLNK + SAL44.DIAFIN;
+            if (parseInt(SAL44.DIAFIN) < parseInt(SAL44.DIAINI)) {
+                _evaluarfechasal_sal44();
+            } else {
+                let URL = get_url("APP/SALUD/SAL44-02.DLL");
+                postData({ datosh: datosEnvio() + SAL44.PREFIJOW + '|' + SAL44.FECHAINICIAL + '|' + SAL44.FECHAFINAL }, URL)
+                    .then(data => {
+                        SAL44.RECONSOLIDACION = data.RECONSOLIDA;
+                        swinvalid = SAL44.RECONSOLIDACION[0].ESTADO;
+                        console.log(SAL44.RECONSOLIDACION, 'SAL44.RECONSOLIDACION')
+                        loader("show");
+                        if (swinvalid == '00') {
+                            var CONTAITEM = 0; 
+                            for (var i in SAL44.RECONSOLIDACION) {
+                                SAL44.RECONSOLIDACION[i].CLIND == '0'?CONTAITEM++:console.log(CONTAITEM, 'CONTAITEM');
+                                if ((SAL44.PREFIJOW == 'E') || (SAL44.PREFIJOW == 'C')) {
+                                    let datos_envio = datosEnvio() + SAL44.RECONSOLIDACION[i].LLAVEFACT + '|';
+                                    SolicitarDll({ datosh: datos_envio }, data => {
+                                        console.log(data, 'INV020');
+                                        dato = data.split('|');
+                                        if (dato[0].trim() == '00') {
+                                            _procesandofacturas_SAL44(i); 
+                                        }
+                                        else {
+                                            CON852(dato[0], dato[1], dato[2], _toggleNav);
+                                        }
+                                    }, get_url('APP/SALUD/SAL020.DLL'));
 
-            SAL44.FECHAFINAL = momentMaskfechafinal.unmaskedValue;
-            console.log(SAL44.FECHAFINAL, 'SAL44.FECHAFINAL')
-            if(( parseInt(SAL44.FECHAFINAL)) > (parseInt(SAL44.FECHAINICIAL))){
-                console.log('fecha inicial menor a fecha final')
-                _evaluarfechasal_sal44(); 
-            }else{
-                console.log('va bien')
-                _leerfacturas_sal44(); 
+                                } else if ($_NITUSU == '0830092718') {
+                                    let datos_envio = datosEnvio() + SAL44.RECONSOLIDACION[i].LLAVEFACT + '|';
+                                    SolicitarDll({ datosh: datos_envio }, data => {
+                                        console.log(data, 'INV020', '0830092718');
+                                        dato = data.split('|');
+                                        if (dato[0].trim() == '00') {
+                                            console.log('sale en el inv020 NIT USU')
+                                        }
+                                        else {
+                                            CON852(dato[0], dato[1], dato[2], _toggleNav);
+                                        }
+                                    }, get_url('APP/SALUD/SAL020.DLL'));
+                                }else{
+                                    _procesandofacturas_SAL44(i);
+                                }      
+                            }
+                            _borrarinput_SAL44()
+                        } else {
+                            $('#prefijo_9441').val('');
+                            $('#descrippref_9441').val('');
+                            $('#anoini_9441').val('');
+                            $('#mesini_9441').val('');
+                            $('#diaini_9441').val('');
+                            $('#anofin_9441').val('');
+                            $('#mesfin_9441').val('');
+                            $('#diafin_9441').val('');
+                            CON851('01', '01', null, 'error', 'error');
+                            _evaluarprefijofact_sal44();
+                        }
+                        loader("hide");
+                    })
+                    .catch(err => {
+                        console.debug(err);
+                    })
             }
         }
     )
 }
-function _leerfacturas_sal44(){
-    console.log('consulta de leer facturas')
+
+function _procesandofacturas_SAL44(i){
+    console.log('procesandofacturas')
+    var CONTAITEM2 = 0;
+    if ($_INVENTUSU == 'S' && SAL44.RECONSOLIDACION[i].CLIND == '0' || SAL44.RECONSOLIDACION[i].MACROIND == '1' && parseInt(SAL44.RECONSOLIDACION[i].INVEIND) > 0) {
+        console.log('entro procesandofacturas')
+        SAL44.SWRECAL = '0';
+        CONTAITEM2++;
+        let datos_envio = datosEnvio() + '|' + SAL44.LLAVEFACT + '|' + SAL44.SWRECAL + '|';
+        SolicitarDll({ datosh: datos_envio }, data => {
+            console.debug(data, 'respuesta');
+            // $('#').val(CONTAITEM);
+            // $('#').val(CONTAITEM2);
+            let datos_envio = datosEnvio() + '|' + SAL44.LLAVEFACT + '|';
+            SolicitarDll({ datosh: datos_envio }, data => {
+                console.debug(data);
+                var date = data.split('|');
+                SAL44.SWBUSCAR = date[0].trim();
+                if (SAL44.SWBUSCAR == '0' && SAL44.RECONSOLIDACION[i].CLIND == '0') {
+                    CON851('3M', '3M', null, 'error', 'error');
+                    console.log('2 no actualizo inventarios')
+                } else {
+                    console.log('2 si actualizo inventarios')
+                }
+            }, get_url('APP/SALUD/SAL030V.DLL'));
+        }, get_url('APP/SALUD/SAL030.DLL'));
+    }else{
+        console.log('finaliza')
+    }
+}
+
+
+function _borrarinput_SAL44() {
+    _inputControl('reset');
+    _inputControl('disabled');
+    _evaluarprefijofact_sal44();
 }
 
 
@@ -207,75 +277,3 @@ var $_FECHAACTUAL = moment().format('YYYYMMDD');
 $_ANOACTUALW = $_FECHAACTUAL.substring(0, 4);
 $_MESACTUALW = $_FECHAACTUAL.substring(4, 6);
 $_DIAACTUAL = $_FECHAACTUAL.substring(6, 8);
-
-var momentFormatfechaini = "YYYY-MM-DD";
-
-var momentMaskfechainicial = IMask($("#fechaini_9441")[0], {
-    mask: Date,
-    pattern: momentFormatfechaini,
-    lazy: true,
-    min: new Date(2019, 0, 1),
-    max: new Date(2025, 0, 1),
-
-    format: function (date) {
-        return moment(date).format(momentFormatfechaini);
-    },
-    parse: function (str) {
-        return moment(str, momentFormatfechaini);
-    },
-    blocks: {
-        YYYY: {
-            mask: IMask.MaskedRange,
-            from: $_ANOLNK,
-            to: $_ANOLNK 
-        },
-        MM: {
-            mask: IMask.MaskedRange,
-            from: $_MESLNK,
-            to: $_MESLNK
-        },
-        DD: {
-            mask: IMask.MaskedRange,
-            from: 1,
-            to: 31
-        }
-    }
-
-});
-
-var momentMaskfechafinal = IMask($("#fechafin_9441")[0], {
-    mask: Date,
-    pattern: momentFormatfechaini,
-    lazy: true,
-    min: new Date(2019, 0, 1),
-    max: new Date(2025, 0, 1),
-
-    format: function (date) {
-        return moment(date).format(momentFormatfechaini);
-    },
-    parse: function (str) {
-        return moment(str, momentFormatfechaini);
-    },
-    blocks: {
-        YYYY: {
-            mask: IMask.MaskedRange,
-            from: $_ANOLNK,
-            to: $_ANOLNK 
-        },
-        MM: {
-            mask: IMask.MaskedRange,
-            from: $_MESLNK,
-            to: $_MESLNK
-        },
-        DD: {
-            mask: IMask.MaskedRange,
-            from: 1,
-            to: 31
-        }
-    }
-
-});
-
-
-
-
