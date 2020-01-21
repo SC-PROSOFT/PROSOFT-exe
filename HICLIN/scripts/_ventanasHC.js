07e28ac9df216adfb73d089280dcda587d25321e
Pablo_Andres_Olg <36211860+AndresOlg@users.noreply.github.com>
2020-01-21 15:03:35 -0500

    Avances SAL451 y SER880

--- HICLIN/scripts/_ventanasHC.js
+++ HICLIN/scripts/_ventanasHC.js
@@ -1,79 +1,118 @@
-﻿function HC810(esccallback, callback) {
-	_toggleF8([{ input: 'atendidos', app: 'SER880', funct: evaluarFNF_HC810 }]);
+﻿/*
+17/01/2020 PO:Pablo Olguin-> Creacion _venatanasHC 
+20/01/2020 PO:Pablo Olguin 
+*/
+
+function HC810(esccallback, callback) {
+	_toggleF8([
+		{ input: 'atendidos', app: 'HC810', funct: evaluarFNF_HC810 },
+		{ input: 'paciente', app: 'HC810', funct: evaluarFNF_HC810 }
+	]);
 	let plantilla =
 		'<div class="col-md-12">'
-		+ '<div class="col-md-4 col-sm-4 col-xs-4">'
+		+ '<div class="col-md-6 col-sm-6 col-xs-6"data-orden="1" id="atendidosHC810">'
 		+ '<label class="col-md-12 col-xs-12 col-sm-12">Desea ver los ya atendidos?</label>'
-		+ '<input class="form-control col-md-12 col-xs-12 col-sm-12" id="atendidos_SER880">'
+		+ '<input class="form-control col-md-12 col-xs-12 col-sm-12" id="atendidos_HC810" max-lenght="1">'
 		+ '</div>'
 		+ '<div class="salto-linea"></div>'
+		+ '<div class="col-md-6 col-sm-6 col-xs-6" id="fechaHC810">'
+		+ '<label class="col-md-12 col-xs-12 col-sm-12">Buscar por fecha?</label>'
+		+ '<input class="form-control col-md-12 col-xs-12 col-sm-12" id="fecha_HC810">'
+		+ '</div>'
+		+ '<div class="col-md-6 col-sm-6 col-xs-6" id="pacienteHC810">'
+		+ '<label class="col-md-12 col-xs-12 col-sm-12">Buscar por Paciente?</label>'
+		+ '<input class="form-control col-md-12 col-xs-12 col-sm-12" id="paciente_HC810">'
+		+ '</div>'
 		+ '</div>'
 		;
-	bootbox.dialog({
+	const ventana_HC810 = bootbox.dialog({
 		message: plantilla,
-		size: 'large',
-		buttons: {
-			confirm: {
-				label: 'SI',
-				className: 'btn-primary'
-			},
-			cancel: {
-				label: 'NO',
-				className: 'btn-danger'
-			}
-		},
-		// callback: function (result) { result == true ? pAtendidos = 'S' : pAtendidos = 'N'; evaluarFNF_HC810(pAtendidos); }
+		size: 'small',
+		buttons: null
 	})
+	ventana_HC810.init(
+		function validarAtendidosHC810() {
+			var idhistoriafactMask = IMask($('#paciente_HC810')[0], { mask: Number, thousandsSeparator: ',', padFractionalZeros: false, normalizeZeros: true, radix: ',', mapToRadix: ['.'] });
+			var fechaDesdeMask = IMask($("#fecha_HC810")[0], {
+				mask: Date,
+				pattern: 'YYYY/MM/DD',
+				lazy: true,
+				blocks: {
+					YYYY: {
+						mask: IMask.MaskedRange,
+						placeholderChar: 'y',
+						from: 2000,
+						to: 2030,
+						maxLength: 4
+					},
+					MM: {
+						mask: IMask.MaskedRange,
+						placeholderChar: 'M',
+						from: 01,
+						to: 12,
+						maxLength: 2,
+					},
+					DD: {
+						mask: IMask.MaskedRange,
+						placeholderChar: 'd',
+						from: 01,
+						to: 31,
+						maxLength: 2,
+					},
+				},
+				format: function (date) { return moment(date).format("YYYY/MM/DD"); },
+				parse: function (str) { return str }
+			});
+			validarInputs({
+				form: "#atendidos_HC810",
+				orden: orden
+			}, validarAtendidosHC810, () => {
+				const atend = document.getElementById('atendidos_HC810').value.toString().toUppercase();
+				if (atend == '') CON851('03', '03', validarAtendidosHC810(), 'error', 'error');
+				else if (['N', 'S'].find(opc => opc == atend) != -1) {
+					evaluarFNTriage(
+						{
+							tecla: null, caja: $('#atendidos_HC810'),
+							cancel: validarAtendidosHC810,
+							acept: onPacientesTriageHC810
+						}
+					);
+				} else CON851('03', '03', validarAtendidosHC810(), 'error', 'error');
+			})
+		});
+	ventana_HC810.on('shown.bs.modal', function () {
+		$("#atendidos_HC810").focus();
+	});
 }
 
 function evaluarFNF_HC810(e) {
-	const plantilla = '<input id="atendidos_HC810" hidden>'
-		// *BUSCA EN TRIAGE POR OTRA FECHA
-		+ '<span>Presione <stong>F2</strong> Buscar por otra fecha</span>'
-		+ '</br>'
-		// *BUSCA EN TRIAGE POR PACIENTE
-		+ '<span>Presione <stong>F8</strong> Buscar por Paciente</span>'
-		+ '</br>'
-		// *MUESTRA LOS MENSAJES POR PACIENTE
-		+ '<span>Presione <stong>F9</strong> Buscar por Paciente</span>'
-		+ '</br>'
-		// *INGRESA A LA LISTA DE ESPERA
-		+ '<span>Presione <stong>F10</strong> Buscar por Paciente</span>'
-	bootbox.dialog({
-		message: plantilla,
-		size: 'small',
-		closeButton: false,
-		buttons: {
-			confirm: {
-				label: 'Aceptar',
-				className: 'btn-primary'
-			}
-		},
-		callback: function (result) { result == true ? pAtendidos = 'S' : pAtendidos = 'N'; evaluarCasoTriage(pAtendidos); },
-		onShow: function () {
-			_toggleF8([{ input: 'atendidos', app: 'HC810', funct: get_datosAtendidos_HC810 }]);
-			$('#atendidos_HC810').focus()
-		}
-	})
-}
-
-function get_datosAtendidos_HC810(e) {
-	if (e.type == "keydown") {
-		switch (e.which) {
-			case 113:
-				bootbox.prompt({
-					title: "Ingrese la fecha de atención a consultar",
-					inputType: 'date',
-					size: 'small',
-					locale: 'custom',
-					callback: function (result) {
-						console.debug(result, 'fecha')
-					}
-				});
-				break;
-
-			default:
-				break;
-		}
+	// FUNCIONES FN-F8 BUSCAR EN TRIAGE POR PACIENTE
+	if (e.type == "keydown" && e.which == 119 || e.type == 'click') {
+		parametros = {
+			dll: 'PACIENTES',
+			valoresselect: ['Nombre del paciente', 'identificación'],
+			f8data: 'PACIENTES',
+			columnas: [{ title: 'COD' }, { title: 'NOMBRE' }, { title: 'EPS' }, { title: 'EDAD' }],
+			callback: data => { idhistoriafactMask.typedValue = data.COD; _enterInput('#paciente_HC810'); },
+			cancel: () => { $('#paciente_HC810').focus(); }
+		};
+		F8LITE(parametros);
+		// FUNCIONES FN-F2 BUSCAR EN TRIAGE POR FECHA
+	} else if (e.type == "keydown" && e.which == 113 || e.type == 'click') {
+		evaluarFNTriage(
+			{
+				tecla: 'F2', caja: $('#fecha_HC810'),
+				cancel: validarAtendidosHC810,
+				acept: onPacientesTriageHC810
+			})
+	}
+	// FUNCIONES FN-F9 IMPRIMIR DE TRIAGE DESDE | HASTA
+	else if (e.type == "keydown" && e.which == 120 || e.type == 'click') {
+		evaluarFNTriage(
+			{
+				tecla: 'F9', caja: null,
+				cancel: validarAtendidosHC810,
+				acept: onPacientesTriageHC810
+			})
 	}
 }
---
+++ SALUD/scripts/SER880.js
@@ -0,0 +1,179 @@
+﻿/*CREACION TURNO TRIAGE*/
+/*2020/01/14 PO - PABLO OLGUIN -> CREACION TURNO TRIAGE*/
+
+var SER880 = new Array(); SER880.PACI = [];
+//--------------------------- MASCARAS ------------------------------//
+var idhistoriafactMask = IMask($('#paci_SER880')[0], { mask: Number, thousandsSeparator: ',', padFractionalZeros: false, normalizeZeros: true, radix: ',', mapToRadix: ['.'] });
+
+$(document).ready(function () {
+	loader('hide'); _inputControl('reset'); _inputControl('disabled');
+	_toggleF8([{ input: 'paci', app: 'SER880', funct: FNFPacientes_SER880 }]);
+	validarPaciente_SER880();
+})
+//----------------------------- FNF8 FUNCIONES -----------------------//
+function FNFPacientes_SER880(e) {
+	// FUNCIONES FN-F8
+	if (e.type == "keydown" && e.which == 119 || e.type == 'click') {
+		parametros = {
+			dll: 'PACIENTES',
+			valoresselect: ['Nombre del paciente'],
+			f8data: 'PACIENTES',
+			columnas: [{ title: 'COD' }, { title: 'NOMBRE' }, { title: 'EPS' }, { title: 'EDAD' }],
+			callback: data => { idhistoriafactMask.typedValue = data.COD; _enterInput('#paci_SER880'); },
+			cancel: () => { $('#paci_SER880').focus(); }
+		};
+		F8LITE(parametros);
+		// FUNCIONESFN-F2
+	} else if (e.type == "keydown" && e.which == 113 || e.type == 'click') { HC810(validarPaciente_SER880, cargarCampos_SER880); }
+	// FUNCIONES FN-F4
+	else if (e.type == "keydown" && e.which == 115 || e.type == 'click') { jAlert({ titulo: 'Advertencia ', mensaje: 'LLama al SER810W [Fase: Desarrollo]' }, validarPaciente_SER880) }
+
+}
+function validarPaciente_SER880() {
+	validarInputs({
+		form: "#paciSER880",
+		orden: '1'
+	}, _toggleNav, () => {
+		const idpaci = idhistoriafactMask.unmaskedValue;
+		var datos_envio = datosEnvio() + idpaci.toString().padStart(15, '0');
+		postData({
+			datosh: datos_envio
+		}, get_url("APP/SALUD/SER810-1.DLL"))
+			.then((data) => {
+				if (data['REG-PACI'][0].COD.trim() == '') { CON851('01', '01', validarPaciente_SER880(), 'error', 'error'); }
+				else {
+					bootbox.confirm({
+						size: "small",
+						message: 'El código digitado no existe, ¿Desea crearlo?',
+						animate: false,
+						callback: function (result) { /* result is a boolean; true = OK, false = Cancel*/
+							if (result == undefined) { validarPaciente_SER880(); }
+							else {
+								if (result == true) {
+									let { ipcRenderer } = require("electron");
+									ipcRenderer.send('another', 'SALUD/PAGINAS/SAL7767.html');
+									vector = ['on', 'Creando nuevo paciente...']
+									_EventocrearSegventana(vector, validarPaciente_SER880);
+								}
+								else { validarPaciente_SER880() }
+							}
+						}
+					})
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+			});
+	})
+}
+function cargarCampos_SER880() {
+	SER880.PACI.EDAD = calcular_edad(SER880.PACI.NACIM);
+	SER880.PACI.SEXO == '' ? document.getElementById('sexopaci_SER880').value = ' ' : document.getElementById('sexopaci_SER880').value = SER880.PACI.SEXO;
+	SER880.PACI.EDAD == '' ? document.getElementById('edadpaci_SER880').value = ' ' : document.getElementById('edadpaci_SER880').value = SER880.PACI.EDAD.vlr_edad + ' ' + SER880.PACI.EDAD.unid_edad;
+	SER880.PACI.DERECHO == '' ? document.getElementById('estadopaci_SER880').value = ' ' : document.getElementById('estadopaci_SER880').value = SER880.PACI.DERECHO + ' ' + consultder_paci(SER880.PACI.DERECHO);
+	SER880.PACI['APELL-PACI1'] == '' ? document.getElementById('apell1_SER880').value = ' ' : document.getElementById('apell1_SER880').value = SER880.PACI['APELL-PACI1'];
+	SER880.PACI['APELL-PACI2'] == '' ? document.getElementById('apell2_SER880').value = ' ' : document.getElementById('apell2_SER880').value = SER880.PACI['APELL-PACI2'];
+	SER880.PACI['NOM-PACI1'] == '' ? document.getElementById('nomb1_SER880').value = ' ' : document.getElementById('nomb1_SER880').value = SER880.PACI['NOM-PACI1'];
+	SER880.PACI['NOM-PACI2'] == '' ? document.getElementById('nomb2_SER880').value = ' ' : document.getElementById('nomb2_SER880').value = SER880.PACI['NOM-PACI2'];
+
+	let ent1 = '', ent2 = '';
+	SER880.PACI['EPS'] == '' ? ent1 = ' ' : ent1 = SER880.PACI['EPS'];
+	SER880.PACI['NIT-FACT'] == '' ? ent2 = ' ' : ent2 = SER880.PACI['NIT-FACT'];
+
+	let datos_envio1 = datosEnvio() + localStorage['Usuario'] + '|' + ent1;
+	SolicitarDll({ datosh: datos_envio1 }, data => {
+		let res = data.split('|');
+		if (res[0] = '00') { document.getElementById('entidafil_SER880').value = res[1] } else { document.getElementById('entidafil_SER880').value = '' }
+	}, get_url('APP/CONTAB/CON110C_07.DLL'));
+
+	//Entidad facturación
+	let datos_envio2 = datosEnvio() + localStorage['Usuario'] + '|' + ent2;
+	SolicitarDll({ datosh: datos_envio2 }, data => {
+		let res = data.split('|');
+		if (res[0] = '00') { document.getElementById('entidafact_SER880').value = res[1]; } else { document.getElementById('entidafact_SER880').value = ' '; }
+	}, get_url('APP/CONTAB/CON110C_01.DLL'));
+	validarTrauma_SER880();
+}
+
+function validarTrauma_SER880() {
+	validarInputs({
+		form: "#traumSER880",
+		orden: '1'
+	}, validarTrauma_SER880, () => {
+		const pTrauma = document.getElementById('traum_SER880').value.toString().toUppercase();
+		if (pTrauma == '') CON851('03', '03', validarTrauma_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pTrauma) != -1) validarRemitido_SER880();
+		else CON851('03', '03', validarTrauma_SER880(), 'error', 'error');
+	})
+}
+
+function validarRemitido_SER880() {
+	validarInputs({
+		form: "#remitidoSER880",
+		orden: '1'
+	}, validarRemitido_SER880, () => {
+		const pRemitido = document.getElementById('remitido_SER880').value.toString().toUppercase();
+		if (pRemitido == '') CON851('03', '03', validarRemitido_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pRemitido) != -1) validarMedicolegal_SER880();
+		else CON851('03', '03', validarRemitido_SER880(), 'error', 'error');
+	})
+}
+
+function validarMedicolegal_SER880() {
+	validarInputs({
+		form: "#medlegalSER880",
+		orden: '1'
+	}, validarMedicolegal_SER880, () => {
+		const pMedlegal = document.getElementById('medlegal_SER880').value.toString().toUppercase();
+		if (pMedlegal == '') CON851('03', '03', validarMedicolegal_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pMedlegal) != -1) validarReingreso_SER880();
+		else CON851('03', '03', validarMedicolegal_SER880(), 'error', 'error');
+	})
+}
+
+function validarReingreso_SER880() {
+	validarInputs({
+		form: "#reingrSER880",
+		orden: '1'
+	}, validarReingreso_SER880, () => {
+		const pReingreso = document.getElementById('reingr_SER880').value.toString().toUppercase();
+		if (pReingreso == '') CON851('03', '03', validarReingreso_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pReingreso) != -1) validarTurno_SER880();
+		else CON851('03', '03', validarReingreso_SER880(), 'error', 'error');
+	})
+}
+
+function validarTurno_SER880() {
+	validarInputs({
+		form: "#turnoSER880",
+		orden: '1'
+	}, validarTurno_SER880, () => {
+		const pTurno = document.getElementById('turno_SER880').value.toString().toUppercase();
+		if (pTurno == '') CON851('03', '03', validarTurno_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pTurno) != -1) validarEmbar_SER880();
+		else CON851('03', '03', validarTurno_SER880(), 'error', 'error');
+	})
+}
+
+function validarEmbar_SER880() {
+	validarInputs({
+		form: "#embarSER880",
+		orden: '1'
+	}, validarTurno_SER880, () => {
+		const pEmbar = document.getElementById('embar_SER880').value.toString().toUppercase();
+		if (pEmbar == '') CON851('03', '03', validarEmbar_SER880(), 'error', 'error');
+		else if (['N', 'S'].find(opc => opc == pEmbar) != -1) HC810();
+		else CON851('03', '03', validarEmbar_SER880(), 'error', 'error');
+	})
+}
+
+function on_restriccionesSER880() {
+switch (key) {
+	case value:
+		
+		break;
+
+	default:
+		break;
+}
+}
